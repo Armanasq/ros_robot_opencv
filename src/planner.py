7d29e932eb344bf6e1b545363b4d368c02de0628
Arman Asgharpoor <60850934+Armanasq@users.noreply.github.com>
2022-04-04 18:21:35 +0430

    Update planner.py

--- src/planner.py
+++ src/planner.py
@@ -1,16 +1,20 @@
 import rospy
 import cv2
+import time
+
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 
 bridge = CvBridge()
 command_pub = rospy.Publisher("motor_commands",String)
-
+h = 600
+l = 375
+r = 425
 def isWhite(val):
  return val > 100
+ 
 def plan(left, right):
-
   threshold_val = 120
   command = "STOP"
   if isWhite(left) and not isWhite(right):
@@ -40,32 +44,34 @@ def plan(left, right):
   return command
   
 def imgCallback(data):
+  global r, l
   try:
     cv_image = bridge.imgmsg_to_cv2(data,"bgr8")
     print(cv_image.shape)
-    l = 300
-    r = 500
+
     gray_image = cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY)
-    command = plan(gray_image[650][l], gray_image[650][r])
+    command = plan(gray_image[h][l], gray_image[h][r])
     if command == "STOP":
-      if r < 800 and l > 0:
-        r = r+50
-        l = l-50
-        command = plan(gray_image[650][r], gray_image[650][l])
-      else:
-        command_pub.publish("BACK")
+      print ("SSSS")
+      for l in range (0,800):
+        r = r+25
+        l = l-25
+        command = plan(gray_image[h][r], gray_image[h][l])
+        if command == "STOP":
+          command_pub.publish("BACK")
+          time.sleep(0.5)
 
         
     
-    gray_image = cv2.line(gray_image, (300,650), (500,650), 0 , 5)
+    gray_image = cv2.line(gray_image, (300,h), (500,h), 0 , 5)
     if command == "RIGHT":
-      gray_image = cv2.circle(gray_image, (500, 650), 5, 255 ,2)    
+      gray_image = cv2.circle(gray_image, (500, h), 5, 255 ,2)    
     if command == "LEFT":
-      gray_image = cv2.circle(gray_image, (300, 650), 5, 255 ,2)    
+      gray_image = cv2.circle(gray_image, (300, h), 5, 255 ,2)    
     if command == "GO":
-      gray_image = cv2.circle(gray_image, (400, 650), 5, 255 ,2)
+      gray_image = cv2.circle(gray_image, (400, h), 5, 255 ,2)
     if command == "BACK":
-      gray_image = cv2.circle(gray_image, (400, 650), 5, 255 ,5)
+      gray_image = cv2.circle(gray_image, (400, h), 5, 255 ,5)
     cv2.imshow("Raw Image", gray_image)
     cv2.waitKey(3)
     
